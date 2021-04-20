1d47692c0a33dd2af42a92e650bb62349a640530
giodude23 <nlbugtong@gmail.com>
2021-04-21 00:03:02 +0800

    Add clear grid button

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+const gridContainer = document.querySelector("#grid-container");
 function createGrid(rows, columns) {
-    const gridContainer = document.querySelector("#grid-container");
-   
+    gridContainer.style.cssText = `grid-template-rows: repeat(${rows}, 1fr); grid-template-columns: repeat(${columns}, 1fr);`;
     for (let i = 0; i < rows * columns; i++) {
         gridContainer.innerHTML += '<div class="grid-item"></div>';
     }
@@ -8,20 +8,49 @@ function createGrid(rows, columns) {
 createGrid(16, 16);
 
 const gridItems = document.querySelectorAll("#grid-container div");
+function addHover(){
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].addEventListener("mouseenter", function(){
+            this.style.backgroundColor = "rgb(65, 65, 65)";
+        });
+    }
+}
+addHover()
 
-for (let i = 0; i < gridItems.length; i++) {
+const btnNew = document.querySelector("#btn-new");
+btnNew.addEventListener("click", newGrid);
+
+function newGrid() {
+    let gridItems = document.querySelectorAll("#grid-container div");
+    let newGrid = prompt("Enter new grid size:", ""); 
+    if (newGrid > 100) {
+        return alert("Grid is too big!");
+    } else if (newGrid == null) {
+        return;
+    }
+
+    // console.log(newGrid);
+    // console.log(typeof parseInt(newGrid));
+
+    for (let i = 0; i < gridItems.length; i++) {
+        gridContainer.removeChild(gridItems[i]);
+}
+createGrid(newGrid, newGrid);
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].setAttribute("style", "background-color: none;");
+ }
+    for (let i = 0; i < gridItems.length; i++) {
     gridItems[i].addEventListener("mouseenter", function(){
         this.style.backgroundColor = "rgb(65, 65, 65)";
     });
 }
+}
 
+const btnClear = document.querySelector("#btn-clear");
+btnClear.addEventListener("click", clearGrid);
 
-
-
-// gridItems.addEventListener("mouseenter", function(){
-//     gridItems.classList.add("hover-color");
-// });
-// gridItems.addEventListener("mouseleave", function(){
-//     setTimeout(function()
-//     {this.style.cssText = "background-color: none";
-// }, 700);});
+function clearGrid() {
+    for (let i = 0; i < gridItems.length; i++) {
+        gridItems[i].setAttribute("style", "background-color: none;");
+ }
+}
